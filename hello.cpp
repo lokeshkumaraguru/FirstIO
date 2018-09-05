437c693c9b05ce357170c8d34c917bef7932c84d
lokeshkumaraguru <lkumaraguru@mail.csuchico.edu>
2018-09-05 16:24:29 -0700

    Changes the greeting

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,11 @@
+//Author:  Lokesh Kumaraguru.
+
 #include <iostream>
 #include <string>
 
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hey, my name is Lokesh Kumaraguru. What's yours?" << std::endl;
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
     return 0;
